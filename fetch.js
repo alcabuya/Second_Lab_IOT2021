6e26f811a84d8d98215eab8f74874de158750c7d
Alberto Cabuya <alcabuyapre@gmail.com>
2021-09-23 01:03:53 +0200

    Sorted table

--- fetch.js
+++ fetch.js
@@ -1,27 +1,27 @@
 var subm = document.getElementById("submit");
 var container = document.getElementById("datafetched");
-var table = document.getElementById("datadisp");
+
 //whenever the button with the id submit is clicked it will execute the function fetch 
 subm.addEventListener('click', dofetch,true);
 
 function dofetch(){
     var quantity = document.getElementById("register_quant");
     //console.log(quantity.value);
-    deleteTable();
+    deleteTableContent();
     displayheader();
     
     document.getElementById("datafetched");
-        for(i=0;i<quantity.value;i++){
-            console.log(i);
-            fetch('https://jsonplaceholder.typicode.com/photos/'+(i+1))
-        .then(response => response.json())
-        .then(json => /*console.log(json)*/ displayjson(json))
-        .catch(function(err){
-            console.log(err);
-        });
-    }    
+       
+            
+            fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(response => response.json())
+            .then(json =>  displayjson(json, quantity.value));        
+      
+     
+
 }
 function displayheader(){
+    var table = document.getElementById("datadisp");
     //console.log(table);
     var newRow = table.insertRow();
     
@@ -34,18 +34,24 @@ function displayheader(){
         newCells[i].innerHTML = `<th><h1>${texts[i]}</h1></th>`;
     
     }
-}
-function displayjson(jsondata){
-    var newRow = table.insertRow();
-    var IdCell = newRow.insertCell();
-    IdCell.innerHTML = `<td>${jsondata.id}</td>`;
-    var ImgCell = newRow.insertCell();
-    ImgCell.innerHTML = ` <td><img src = ${jsondata.url} width="200" height = "200"></td>`;
-    var TittleCell = newRow.insertCell();
-    TittleCell.innerHTML = `<td><b>${jsondata.title}</b></td>`;
     
 }
-function deleteTable(){
+function displayjson(jsondata,quantity){
+    console.log(jsondata[0]);
+    console.log(quantity);
+    for(i = 0;i<quantity;i++){
+        var table = document.getElementById("datadisp");
+        var newRow = table.insertRow();
+        var IdCell = newRow.insertCell();
+        IdCell.innerHTML = `<td><p>${jsondata[i].id}</p></td>`;
+        var ImgCell = newRow.insertCell();
+        ImgCell.innerHTML = ` <td><img src = ${jsondata[i].url} width="200" height = "200"></td>`;
+        var TittleCell = newRow.insertCell();
+        TittleCell.innerHTML = `<td><p><b>${jsondata[i].title}</b></p></td>`;
+    }
+}
+function deleteTableContent(){
+    var table = document.getElementById("datadisp");
     //don't know if it is the optimal solution 
     table.innerHTML = "";
 }
